59d8a67be3d9b3ce524137d5fae26144c6695f78
Yaroslav Matala <s9682642@gmail.com>
2024-11-21 17:23:29 +0000

    add styles

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,6 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = "post_list.html"
+    queryset = Post.objects.filter(status=1)
+    template_name = "blog/index.html"
+    paginate_by = 6
